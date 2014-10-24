bb045e4ad9a3c559a808e92333b5dd6a23f5b15e
Rowan McAllister <mcallr15@polyprep.org>
2014-10-24 17:50:25 +0000

    Initial commit

---
+++ Game.java
@@ -0,0 +1,8 @@
+class Game {
+    public static void main(String args[]){
+        Point pos = new Point();
+        pos.x = 3;
+        pos.y = 4;
+        System.out.println(pos.x + " " + pos.y);
+    }
+}
